a0200a65d4e8c4cabb5c5fb97c4a7c94316861b4
Tombo <tomas.ostatne@proton.me>
2026-04-14 01:27:02 +0200

    1 gunicorn worker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo "Starting application..."
 exec gunicorn \
   -k uvicorn.workers.UvicornWorker \
   app.main:app \
-  -w $(($(nproc) * 2 + 1)) \
+  -w 1 \
   -b 0.0.0.0:8000 \
   --access-logfile - \
   --error-logfile - \
